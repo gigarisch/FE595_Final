5cfb4dd3c889d0cd54cdce4061eaf06edcf6231b
gigarisch <gigarisch@gmail.com>
2019-11-05 11:00:05 -0500

    Updated error message

--- Reboot/main_app.py
+++ Reboot/main_app.py
@@ -7,7 +7,7 @@ app.config["DEBUG"] = True
 
 @app.errorhandler(404)
 def not_found(error):
-    return 'The page you requested could not be found', 404
+    return 'The page you requested could not be found.'
 
 @app.route("/", methods=["GET", "POST"])
 def home():
