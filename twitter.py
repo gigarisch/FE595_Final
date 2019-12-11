0db32c5748e3589f7858b516920a2f53697fe0f0
cstipcak <55324701+cstipcak@users.noreply.github.com>
2019-12-10 18:40:09 -0600

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -17,13 +17,13 @@ def get_create_api():
 
 #String
 def get_string(api, handle):
-	tweets=api.user_timeline(screen_name='@handle',count=200, tweet_mode='extended')
+	tweets=api.user_timeline(screen_name=handle,count=200, tweet_mode='extended')
 	tweet=[tweet.full_text for tweet in tweets]
 	return tweet
 
 #Data table
 def get_table(api, handle):
-	tweets=api.user_timeline(screen_name='@handle',count=200, tweet_mode='extended')
+	tweets=api.user_timeline(screen_name=handle,count=200, tweet_mode='extended')
 	frame= [[tweet.user.screen_name, tweet.user.location, tweet.full_text,tweet.created_at]for tweet in tweets]
 	tweet_frame = pd.DataFrame(data=frame,
                            columns=['user',"location","text","created_at"])
